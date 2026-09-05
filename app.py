2c2d8695746c571329a8df052c1aa3b1745677f6
mohamedaty20 <86588628+mohamedaty20@users.noreply.github.com>
2026-09-05 19:05:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ def run_ai_auditor_module():
                         contents = [prompt, image_part]
 
                     response = client.models.generate_content(
-                        model="gemini-2.5-flash",
+                        model="gemini-3.5-flash-lite",
                         contents=contents,
                         config=types.GenerateContentConfig(
                             thinking_config=types.ThinkingConfig(thinking_level=types.ThinkingLevel.LOW)
