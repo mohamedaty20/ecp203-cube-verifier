38dfdf7ba687c652001942684b634ca3b16cc2e3
mohamedaty20 <86588628+mohamedaty20@users.noreply.github.com>
2026-09-05 16:26:19 +0300

    app.py

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ def run_ai_auditor_module():
                     from google.genai import types
 
                     response = client.models.generate_content(
-                        model='gemini-3.6-flash',
+                        model='gemini-1.5-flash',
                         contents=[
                             prompt,
                             types.Part.from_bytes(
