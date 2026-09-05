637e4e8f3e8d6dced758b7a0e46646835e49e509
mohamedaty20 <86588628+mohamedaty20@users.noreply.github.com>
2026-09-06 01:57:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -458,7 +458,7 @@ def run_ai_auditor_module():
                         contents = [prompt, image_part]
 
                     response = client.models.generate_content(
-                        model="gemini-2.5-flash",
+                        model="gemini-3.5-flash-lite",
                         contents=contents,
                     )
                     
@@ -553,7 +553,7 @@ def run_crack_defect_analyzer():
                     
                     image_part = types.Part.from_bytes(data=defect_image.getvalue(), mime_type=defect_image.type)
                     response = client.models.generate_content(
-                        model="gemini-2.5-flash",
+                        model="gemini-3.5-flash-lite",
                         contents=[prompt, image_part]
                     )
                     
@@ -654,7 +654,7 @@ def run_core_code_chatbot():
                         full_prompt = f"{system_context}\n\nUser Question: {user_query}"
                         
                         response = client.models.generate_content(
-                            model="gemini-2.5-flash",
+                            model="gemini-3.5-flash-lite",
                             contents=full_prompt,
                         )
                         response_text = response.text
